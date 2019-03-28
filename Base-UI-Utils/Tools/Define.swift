c102c01c8d469f4fd1ecfd38429ff5816ff89d22
LangFZ <446003664@qq.com>
2019-03-28 23:29:28 +0800

    fix 全局常量改为NSObject类扩展计算属性

--- Base-UI-Utils/Tools/Define.swift
+++ Base-UI-Utils/Tools/Define.swift
@@ -8,74 +8,77 @@
 
 import UIKit
 
-public let kScreen: CGRect = UIScreen.main.bounds
-public let kScreenW: CGFloat = kScreen.size.width
-public let kScreenH: CGFloat = kScreen.size.height
-
-public let kNaviBarH: CGFloat = getNavigationBarHeight()
-public let kStatusH: CGFloat = getStatusHeight()
-
-public let kTabBarH: CGFloat = getTabBarHeight()
-public let kTabBarBotH: CGFloat = getTabBarBottomHeight()
-
-public func frameMath(_ frame: CGFloat) -> CGFloat {
-    return frame/375.0*UIScreen.main.bounds.width
-}
-
-// MARK: - NavigationBar
-public func getNavigationBarHeight() -> CGFloat {
-    if isIphoneX() {
-        return CGFloat.init(88)
-    } else {
-        return CGFloat.init(64)
-    }
-}
-public func getStatusHeight() -> CGFloat {
-    if isIphoneX() {
-        return CGFloat.init(44)
-    } else {
-        return CGFloat.init(20)
-    }
-}
-
-// MARK: - TabBar
-public func getTabBarHeight() -> CGFloat {
-    if isIphoneX() {
-        return CGFloat.init(83)
-    } else {
-        return CGFloat.init(49)
-    }
-}
-public func getTabBarBottomHeight() -> CGFloat {
-    if isIphoneX() {
-        return CGFloat.init(34)
-    } else {
-        return 0
-    }
-}
-
-// MARK: - 刘海屏
-public func isIphoneX()->Bool {
-    if UIApplication.shared.statusBarFrame.height == 44 {
-        return true
-    } else {
-        return false
-    }
-}
-
-// MARK: - Debug模式下打印
-public func print_debug(_ items: Any..., file:String = #file, funcName:String = #function, lineNum:Int = #line) {
+public extension NSObject {
     
-    #if DEBUG
-    let fileName = (file as NSString).lastPathComponent
+    public var kScreen: CGRect { return UIScreen.main.bounds }
+    public var kScreenW: CGFloat { return kScreen.size.width }
+    public var kScreenH: CGFloat { return kScreen.size.height }
     
-    var text = ""
+    public var kNaviBarH: CGFloat { return getNavigationBarHeight() }
+    public var kStatusH: CGFloat { return getStatusHeight() }
     
-    for item in items {
-        text += "\n"
-        text += "\(item)"
+    public var kTabBarH: CGFloat { return getTabBarHeight() }
+    public var kTabBarBotH: CGFloat { return getTabBarBottomHeight() }
+    
+    public func frameMath(_ frame: CGFloat) -> CGFloat {
+        return frame/375.0*UIScreen.main.bounds.width
     }
     
-    print("\nClass: \(fileName) === line: \(lineNum)\(text)")
-    #endif
+    // MARK: - NavigationBar
+    public func getNavigationBarHeight() -> CGFloat {
+        if isIphoneX() {
+            return CGFloat.init(88)
+        } else {
+            return CGFloat.init(64)
+        }
+    }
+    public func getStatusHeight() -> CGFloat {
+        if isIphoneX() {
+            return CGFloat.init(44)
+        } else {
+            return CGFloat.init(20)
+        }
+    }
+    
+    // MARK: - TabBar
+    public func getTabBarHeight() -> CGFloat {
+        if isIphoneX() {
+            return CGFloat.init(83)
+        } else {
+            return CGFloat.init(49)
+        }
+    }
+    public func getTabBarBottomHeight() -> CGFloat {
+        if isIphoneX() {
+            return CGFloat.init(34)
+        } else {
+            return 0
+        }
+    }
+    
+    // MARK: - 刘海屏
+    public func isIphoneX()->Bool {
+        if UIApplication.shared.statusBarFrame.height == 44 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    // MARK: - Debug模式下打印
+    public func print_debug(_ items: Any..., file:String = #file, funcName:String = #function, lineNum:Int = #line) {
+        
+        #if DEBUG
+        let fileName = (file as NSString).lastPathComponent
+        
+        var text = ""
+        
+        for item in items {
+            text += "\n"
+            text += "\(item)"
+        }
+        
+        print("\nClass: \(fileName) === line: \(lineNum)\(text)")
+        #endif
+    }
 }
