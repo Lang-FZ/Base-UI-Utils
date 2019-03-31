1d239f9bfec4cb21f02cde2e4fc1d1e355f8e602
LangFZ <446003664@qq.com>
2019-03-31 17:25:11 +0800

    0.0.15

--- Base-UI-Utils/Module/PhotoViewer/Model/PhotoViewerPhotoModel.swift
+++ Base-UI-Utils/Module/PhotoViewer/Model/PhotoViewerPhotoModel.swift
@@ -21,23 +21,4 @@ class PhotoViewerPhotoModel: NSObject {
     override init() {
         super.init()
     }
-    
-    public func testModel() {
-        
-        let url1 = PhotoViewerPhotoModel()
-        url1.photoUrl = URL.init(string: "http://img.netbian.com/file/2019/0312/e70d8c660d7321b25096cb7247f9a11a.jpg")
-        photoData.append(url1)
-        
-        let model2 = PhotoViewerPhotoModel()
-        model2.photoName = "bizhi2.jpg"
-        photoData.append(model2)
-        
-        let model6 = PhotoViewerPhotoModel()
-        model6.photoName = "bizhi6.jpg"
-        photoData.append(model6)
-        
-        let model10 = PhotoViewerPhotoModel()
-        model10.photoName = "bizhi10.jpg"
-        photoData.append(model10)
-    }
 }
--- Base-UI-Utils/ViewController.swift
+++ Base-UI-Utils/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.view.backgroundColor = UIColor.cyan
     }
     
@@ -20,7 +19,22 @@ class ViewController: UIViewController {
 //        let touch:UITouch = (((touches as NSSet).anyObject() as AnyObject) as! UITouch)
         
         let photoModel = PhotoViewerPhotoModel.init()
-        photoModel.testModel()
+        
+        let url1 = PhotoViewerPhotoModel()
+        url1.photoUrl = URL.init(string: "http://img.netbian.com/file/2019/0312/e70d8c660d7321b25096cb7247f9a11a.jpg")
+        photoModel.photoData.append(url1)
+        
+        let model2 = PhotoViewerPhotoModel()
+        model2.photoName = "bizhi2.jpg"
+        photoModel.photoData.append(model2)
+        
+        let model6 = PhotoViewerPhotoModel()
+        model6.photoName = "bizhi6.jpg"
+        photoModel.photoData.append(model6)
+        
+        let model10 = PhotoViewerPhotoModel()
+        model10.photoName = "bizhi10.jpg"
+        photoModel.photoData.append(model10)
         
         let photoVC:PhotoViewerController = PhotoViewerController()
         photoVC.data = photoModel
