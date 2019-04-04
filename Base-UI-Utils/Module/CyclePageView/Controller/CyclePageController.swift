31d883de546ee73ef08392342755efaef6589a54
LangFZ <fengzhao@igola.com>
2019-04-04 19:13:07 +0800

    OPT

--- Base-UI-Utils/Module/CyclePageView/Controller/CyclePageController.swift
+++ Base-UI-Utils/Module/CyclePageView/Controller/CyclePageController.swift
@@ -11,16 +11,19 @@ import UIKit
 class CyclePageController: NoneNaviBarController {
     
     // MARK: - 懒加载
+    private lazy var coverFlow: CyclePageCoverFlow = {
+        let coverFlow = CyclePageCoverFlow.init()
+        coverFlow.scrollDirection = .horizontal
+        coverFlow.sectionInset = UIEdgeInsets.init(top: 0, left: 0, bottom: 0, right: 0)
+        coverFlow.minimumLineSpacing = 0
+        coverFlow.minimumInteritemSpacing = 0
+        coverFlow.estimatedItemSize = CGSize.init(width: kScreenW, height: frameMath(140))
+        return coverFlow
+    }()
+    
     private lazy var cycle_page: UICollectionView = {
         
-        let flowLayout = UICollectionViewFlowLayout.init()
-        flowLayout.scrollDirection = .horizontal
-        flowLayout.sectionInset = UIEdgeInsets.init(top: 0, left: 0, bottom: 0, right: 0)
-        flowLayout.minimumLineSpacing = 0
-        flowLayout.minimumInteritemSpacing = 0
-        flowLayout.estimatedItemSize = CGSize.init(width: kScreenW, height: frameMath(140))
-        
-        let cycle_page = UICollectionView.init(frame: CGRect.zero, collectionViewLayout: flowLayout)
+        let cycle_page = UICollectionView.init(frame: CGRect.zero, collectionViewLayout: coverFlow)
         cycle_page.backgroundColor = UIColor.clear
         cycle_page.showsVerticalScrollIndicator = false
         cycle_page.showsHorizontalScrollIndicator = false
@@ -28,7 +31,7 @@ class CyclePageController: NoneNaviBarController {
         cycle_page.dataSource = self
         cycle_page.isPagingEnabled = true
         
-//        cycle_page.register(CarnivalActivitiesCell_Item.self, forCellWithReuseIdentifier: Carnival_Activities_Idenfitier)
+//        cycle_page.register(CycleCyclePageItem.self, forCellWithReuseIdentifier: CycleCyclePageItem_Idenfitier)
         
         return cycle_page
     }()
@@ -36,7 +39,9 @@ class CyclePageController: NoneNaviBarController {
     // MARK: - 系统方法
     override func viewDidLoad() {
         super.viewDidLoad()
-        UICollectionViewFlowLayout.init()
+        view.backgroundColor = UIColor.purple
+        
+        view.addSubview(cycle_page)
     }
     
     override func didReceiveMemoryWarning() {
